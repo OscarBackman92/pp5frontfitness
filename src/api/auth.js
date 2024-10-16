e6c15d8f6142ffc89cc74f190d8e2b4f3a385e77
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-16 21:24:14 +0200

    debugging

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const API_URL = 'https://fitnessapi-d773a1148384.herokuapp.com/api/auth/register/';
+const API_URL = 'https://fitnessapi-d773a1148384.herokuapp.com/api/auth/';
 
 export const login = async (credentials) => {
     const response = await axios.post(`${API_URL}api/auth/login/`, credentials);
     return response.data;
 };
 
-export const register = async (userData) => {
-    const response = await axios.post(`${API_URL}api/auth/register/`, userData);
+export const register = async (data) => {
+    const response = await axios.post('https://fitnessapi-d773a1148384.herokuapp.com/api/auth/register/', data);
     return response.data;
 };
 
